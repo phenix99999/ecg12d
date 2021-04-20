15e9ed188b1a2e65246f4727a29976d12434b831
Lyes Tamazouzt <ltamazouzt@v-hiculemedia.com>
2021-04-20 16:05:42 -0400

    debug

--- src/screens/CarteScreen.tsx
+++ src/screens/CarteScreen.tsx
@@ -196,7 +196,6 @@ const CarteScreen = ({ navigation, authStore }: Props) => {
             getCardInfoConst();
         }
 
-
     }, [noDeCarteAutomatique]);
 
     const handleBarCodeScanned = ({ type, data }) => {
