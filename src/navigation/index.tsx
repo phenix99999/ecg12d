6d76fc4dbb3b91099ef21912578844b6c7566f7e
Lyes Tamazouzt <lyestamazouzt@Lyess-MacBook-Pro.local>
2021-11-12 18:56:34 -0500

    debug

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -100,8 +100,7 @@ export default class App extends Component {
                     </TouchableOpacity>
 
                     <View style={{ flexDirection: 'row', padding: 20, borderBottomWidth: 1, borderColor: '#e2e2e2' }}
-                    >
-                        <Text>Version 2.1.5</Text>
+                    ><Text>Version 2.1.4</Text>
                     </View>
 
                 </View>
